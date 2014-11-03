6e2dc395c5d65b9e9537da4f696ad6df1a7f2edd
TV <mye.mail@gmx.net>
2014-11-03 12:25:19 +0000

    Code cleaning.

--- Greengraph/greengraph.py
+++ Greengraph/greengraph.py
@@ -1,59 +1,67 @@
-### "geolocation"
+"""greengraph.pyplot
+   Calculates the amount of green between London and Birmingham using google maps.
+"""
+
 import geopy
 import requests
+import png
+from itertools import izip
+from numpy import linspace
+from StringIO import StringIO
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
 
-geocoder=geopy.geocoders.GoogleV3(domain="maps.google.co.uk")
+########################################
+
+geocoder = geopy.geocoders.GoogleV3(domain="maps.google.co.uk")
 def geolocate(place):
   return geocoder.geocode(place,exactly_one=False)[0][1]
 
-london_location=geolocate("London")
+london_location = geolocate("London")
 print london_location
 
 ### "URL"
 def map_at(lat, long, satellite=False, zoom=12, size=(400,400), sensor=False):
-    base="http://maps.googleapis.com/maps/api/staticmap?"
-    params=dict(
-        sensor= str(sensor).lower(),
-        zoom= zoom,
-        size= "x".join(map(str,size)),
-        center= ",".join(map(str,(lat,long))),
-        style= "feature:all|element:labels|visibility:off"
-        )
+    base = "http://maps.googleapis.com/maps/api/staticmap?"
+    params = dict(
+        sensor=str(sensor).lower(),
+        zoom=zoom,
+        size="x".join(map(str,size)),
+        center=",".join(map(str,(lat,long))),
+        style="feature:all|element:labels|visibility:off")
     if satellite:
-        params["maptype"]="satellite"
-    return requests.get(base,params=params)
+        params["maptype"] = "satellite"
+    return requests.get(base, params=params)
 
-map_response=map_at(51.5072, -0.1275, zoom=10)
-url=map_response.url
+map_response = map_at(51.5072, -0.1275, zoom=10)
+url = map_response.url
 print url
 
 ### "png"
 def is_green(r, g, b):
-  threshold=1.1
-  return g>r*threshold and g>b*threshold
-
-import png
-from itertools import izip
+  threshold = 1.1
+  return g > r*threshold and g > b*threshold
 
 def count_green_in_png(data):
-    image=png.Reader(file=StringIO(data.content)).asRGB()
+    image = png.Reader(file=StringIO(data.content)).asRGB()
     count = 0
     for row in image[2]:
-        pixels=izip(*[iter(row)]*3)
-        count+=sum(1 for pixel in pixels if is_green(*pixel))
+        pixels = izip(*[iter(row)]*3)
+        count += sum(1 for pixel in pixels if is_green(*pixel))
     return count
 
-from StringIO import StringIO
+
 print count_green_in_png(map_at(*london_location))
 
 ### "visualise"
 def show_green_in_png(data):
-    image=png.Reader(file=StringIO(data.content)).asRGB()
+    image = png.Reader(file=StringIO(data.content)).asRGB()
     count = 0
-    out=[]
+    out = []
     for row in image[2]:
-        outrow=[]
-        pixels=izip(*[iter(row)]*3)
+        outrow = []
+        pixels = izip(*[iter(row)]*3)
         for pixel in pixels:
             outrow.append(0)
             if is_green(*pixel):
@@ -62,23 +70,22 @@ def show_green_in_png(data):
                 outrow.append(0)
             outrow.append(0)
         out.append(outrow)
-    buffer=StringIO()
+    buffer = StringIO()
     result = png.from_array(out,mode='RGB')
     result.save(buffer)
     return buffer.getvalue()
 
 
 ### "points"
-from numpy import linspace
 def location_sequence(start, end, steps):
   """ Would actually prefer this if steps
       were deduced from zoomlevel
       But need projection code for that"""
-  lats=linspace(start[0],end[0],steps)
-  longs=linspace(start[1],end[1],steps)
-  return zip(lats,longs)
+  lats = linspace(start[0], end[0], steps)
+  longs = linspace(start[1], end[1], steps)
+  return zip(lats, longs)
 
-[count_green_in_png(map_at(*location,zoom=10,satellite=True))
+[count_green_in_png(map_at(*location, zoom=10, satellite=True))
             for location in location_sequence(
                 geolocate("London"),
                 geolocate("Birmingham"),
@@ -86,18 +93,14 @@ def location_sequence(start, end, steps):
 
 
 ### "save"
-import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.use('Agg')
-
 with open('green.png','w') as green:
     green.write(show_green_in_png(map_at(*london_location,
-        zoom=10,satellite=True)))
+        zoom=10, satellite=True)))
 
 plt.plot([
     count_green_in_png(
-        map_at(*location,zoom=10,satellite=True))
+        map_at(*location, zoom=10, satellite=True))
           for location in location_sequence(
               geolocate("London"),
-              geolocate("Birmingham"),10)])
+              geolocate("Birmingham"), 10)])
 plt.savefig('greengraph.png')
