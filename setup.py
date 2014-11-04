06981e76a6bbaabedc8609a1c191e2780d215711
TV <mye.mail@gmx.net>
2014-11-04 11:23:09 +0000

    Removed explicit dependencies to standard library modules, and deleted unmatched parenthesis.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(name = 'Greengraph',
       version = '0.1',
       description = 'Green space between London and Birmingham.',
-      packages = find_packages(exclude=['test*'])),
-      install_requires = ['geopy', 'requests', 'pypng', 'numpy', 'matplotlib', 'StringIO', 'url', 'itertools'],
-      scripts = ['scripts/greengraph'])
+      packages = find_packages(exclude=['test*']),
+      install_requires = ['geopy', 'requests', 'pypng', 'numpy', 'matplotlib', 'url']
+      #scripts = ['scripts/greengraph']
+      )
+      
